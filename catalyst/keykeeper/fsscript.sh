43907afe5566a18c31c6da4770234de4b00f7d45
Linus Nordberg <linus@nordberg.se>
2016-11-28 23:19:57 +0100

    Create /root/.ssh/.

--- catalyst/keykeeper/fsscript.sh
+++ catalyst/keykeeper/fsscript.sh
@@ -5,6 +5,7 @@ EOF
 echo 'hostname="keykeeper"' > /etc/conf.d/hostname
 cat /dev/null > /etc/issue
 cp /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
+mkdir /root/.ssh; chmod 700 /root/.ssh
 cat << EOF > /root/.ssh/authorized_keys
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID4bv1RXziZSjHkKY5kDbxboNUGkHEpBivdX8fdvl7Zt
 EOF
