3fdb33a10e96d74466a12f52a4af464f101a0a2f
Linus Nordberg <linus@nordberg.se>
2016-11-29 00:55:00 +0100

    Add catalyst/README; move emerging from install.sh; use newer stage3

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 DIR=$(pwd)
 
 # PCSC-lite && ACS card reader driver
-emerge app-crypt/ccid 
 rc-update add pcscd default 
 (
 tar -C /usr/src -xvz -f $DIR/extras/acsccid-1.1.3.tar.gz 
@@ -13,14 +12,11 @@ cp $DIR/extras/Info.plist /usr/lib64/readers/usb/ifd-acsccid.bundle/Contents/Inf
 )
 
 # HW random number generator support
-emerge sys-apps/rng-tools
-emerge sys-apps/usbutils
 rc-update add rngd default
 cp $DIR/extras/rngd.conf /etc/conf.d/rngd
 cp $DIR/extras/99_ldattach_rngd.rules /etc/udev/rules.d
 
 # Luna SA client
-emerge rpm
 rpm -i --nodeps $DIR/extras/luna/*.rpm
 
 # configure Luna p2p network
@@ -28,9 +24,5 @@ rpm -i --nodeps $DIR/extras/luna/*.rpm
 touch /etc/conf.d/net
 grep -q config_enp2s0 /etc/conf.d/net || echo "config_enp2s0=\"10.0.0.2/255.255.255.0\"" >> /etc/conf.d/net
 
-# python & dependencies (secretsharing)
-emerge dev-python/pip # pulls in python
+# secretsharing
 pip install secretsharing
-
-# openssl
-emerge openssl
