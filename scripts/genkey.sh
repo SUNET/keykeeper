f8bc10ce98099df702b190d2f75277ba0a8fac04
Leif Johansson <leifj@sunet.se>
2016-11-28 20:11:53 +0100

    expiration time

--- scripts/genkey.sh
+++ scripts/genkey.sh
@@ -4,9 +4,10 @@ cd /dev/shm
 name=$1
 def_subject="/CN=${name}"
 subject=${2:-$def_subject}
+days=${3:-"3650"}
 
 pass=`openssl rand -base64 32 | tr -d /=+ | cut -c -40`
 
 openssl genrsa -aes256 -passout pass:${pass} 4096 > ${name}.key
-openssl req -x509 -sha256 -new -subj ${subject} -passin pass:${pass} -key ${name}.key -out ${name}.crt
+openssl req -x509 -sha256 -days $days -new -subj ${subject} -passin pass:${pass} -key ${name}.key -out ${name}.crt
 
