113e267bfef4b0113a8390da29afc4abc05c17b8
Linus Nordberg <linus@nordu.net>
2016-11-29 20:24:02 +0100

    Allow extra arguments to catalyst.

--- catalyst/keykeeper/mkstage4.sh
+++ catalyst/keykeeper/mkstage4.sh
@@ -3,4 +3,4 @@ n=stage4-amd64-keykeeper.201611
 rm -f /var/tmp/catalyst/tmp/hardened/.autoresume-$n/{chroot_setup,setup_confdir,build_packages}
 ln -sf /var/tmp/catalyst/builds/hardened/$n.tar.bz2
 [ -L tmp ] && rm tmp; ln -sf /var/tmp/catalyst/tmp/hardened/$n tmp
-time catalyst -c catalyst.conf -f stage4.spec
+time catalyst -c catalyst.conf -f stage4.spec $@
