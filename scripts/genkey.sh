323855d41fa61dffa32e848cdb6cd8227a6aac90
Fredrik Domeij <fredrik.domeij@umu.se>
2016-11-29 11:12:54 +0100

    extra " removed comment of expiry added

--- scripts/genkey.sh
+++ scripts/genkey.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd /dev/shm
-name=${1:-"md-signer"}
+name=${1:-md-signer}
 def_subject="/C=SE/ST=Stockholm/L=Stockholm/O=SUNET/OU=SWAMID/CN=SWAMID metadata signer v2.0"
 subject=${2:-$def_subject}
-days=${3:-"7305"}
+days=${3:-7305}		# 20 years
 
 pass=`openssl rand -base64 32 | tr -d /=+ | cut -c -40`
 
