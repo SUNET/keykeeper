a7dd2f2bf02c5457287211c58edd409df6c950c3
Fredrik Aslund <fredrik.aslund@umu.se>
2016-11-29 09:16:57 +0100

    default subject and expire time for new signing key added

--- scripts/genkey.sh
+++ scripts/genkey.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd /dev/shm
-name=$1
-def_subject="/CN=${name}"
-subject=${2:-$def_subject}
+
+name="md-signer"
+subject="/C=SE/ST=Stockholm/L=Stockholm/O=SUNET/OU=SWAMID/CN=SWAMID metadata signer v2.0"
+days=7305	# 20 years
 
 pass=`openssl rand -base64 32 | tr -d /=+ | cut -c -40`
 
 openssl genrsa -aes256 -passout pass:${pass} 4096 > ${name}.key
-openssl req -x509 -sha256 -new -subj ${subject} -passin pass:${pass} -key ${name}.key -out ${name}.crt
+openssl req -x509 -sha256 -new -subj "${subject}" -days ${days} -passin pass:${pass} -key ${name}.key -out ${name}.crt
 
