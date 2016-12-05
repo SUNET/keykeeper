6d17875ac7662a808fcbb7c145c3120cf6f7d62b
Leif Johansson <leifj@sunet.se>
2016-12-05 16:06:47 +0100

    apply pyscard patch

--- catalyst/keykeeper/root_overlay/usr/local/keykeeper/bin/install-on-target.sh
+++ catalyst/keykeeper/root_overlay/usr/local/keykeeper/bin/install-on-target.sh
@@ -21,5 +21,6 @@ DIR=/usr/local/keykeeper
 (
     tar -C /usr/src -xf $DIR/distfiles/pyscard-release-1.9.4.tar.gz
     cd /usr/src/pyscard-release-1.9.4
+    patch -p1 < $DIR/distfiles/pyscard.diff
     python2 setup.py install
 )
